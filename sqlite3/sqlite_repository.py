fe52f677789b807c9946ec8e82decaa9f69c38cb
Flep <morteza.aummo@gmail.com>
2026-02-14 19:39:57 +0330

    make just the sqlite class in sqlite_repository file

--- sqlite3/sqlite_repository.py
+++ sqlite3/sqlite_repository.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-class sqlite:
+class TaskDB:
     def __init__(self,title,desc,id):
         self.title = title
         self.desc = desc
@@ -9,21 +9,47 @@ class sqlite:
         connector = sqlite3.connect('task_data_basa.db')
         cur = connector.cursor()
 
-        cur.execute("""""
-        CREATE TABLE IF NOT EXISTS task (
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS tasks (
             title TEXT NOT NULL,
             description TEXT,
-            id TEXT NOT NULL
+            id TEXT  PRIMARY KEY
         )
-        """"")
+        """)
         connector.commit()
+        connector.close()
 
     def write(self):
-        pass
+        connector = sqlite3.connect('task_data_basa.db')
+        cur = connector.cursor()
+        cur.execute(
+            "INSERT INTO tasks (title,description,id) VALUES (?,?,?)",(self.title,self.desc,self.id)
+        )
+        connector.commit()
+        connector.close()
 
-    def read(self):
-        pass
+    def remove(self):
+        connector = sqlite3.connect('task_data_basa.db')
+        cur = connector.cursor()
+        cur.execute("DELETE FROM tasks WHERE id = ?",(self.id,))
+        connector.commit()
+        connector.close()
 
+    def read_one(self):
+        connector = sqlite3.connect('task_data_basa.db')
+        cur = connector.cursor()
+        cur.execute("SELECT * FROM tasks WHERE id = ?", (self.id,))
+        row = cur.fetchone()
+        connector.close()
+        return row
+
+    def read_all(self):
+        connector = sqlite3.connect('task_data_basa.db')
+        cur = connector.cursor()
+        cur.execute("SELECT * FROM tasks")
+        rows = cur.fetchall()
+        connector.close()
+        return rows
     def update(self):
         pass
 
